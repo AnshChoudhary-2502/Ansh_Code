9507a9d4b1a1138aad9066e0b19fa45104cda904
AnshChoudhary-2502 <ansh.choudhary@innvonix.in>
2026-01-05 16:38:20 +0530

    first commit

---
+++ control_flow.py
@@ -0,0 +1,9 @@
+age = int(input("Your age is: "))
+if age >0 and age <18:
+    print("Minor")
+elif age >=18 and age < 50:
+    print("Eligible")
+elif age >= 50:
+    print("Not Eligible")
+else:
+    print("Invalid Age")
