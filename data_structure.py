cc7b95f2de2301007f1d909927aa38544eabfbf6
AnshChoudhary-2502 <ansh.choudhary@innvonix.in>
2026-01-05 18:39:51 +0530

    dsa

---
+++ data_structure.py
@@ -0,0 +1,15 @@
+#list : ordered, allows duplicates, mutable
+my_list = [5,3,1,2,4,5]
+print("List:", my_list)
+
+#tuple : ordered, allows duplicates, immutable
+my_tuple = (1, 2, 3, 4, 5, 5)
+print("Tuple:", my_tuple)
+
+#set : unordered, no duplicates, mutable
+my_set = {1, 2, 3, 4, 5, 5}
+print("Set:", my_set)
+
+#dictionary : unordered, key-value pairs, mutable
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+print("Dictionary:", my_dict)   
