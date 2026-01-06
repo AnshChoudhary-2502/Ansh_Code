d33c4ac1a6c6dc45921a51e0e836a84082f1b6b3
AnshChoudhary-2502 <ansh.choudhary@innvonix.in>
2026-01-06 09:50:11 +0530

    change

--- conditional_statements.py
+++ conditional_statements.py
@@ -1,9 +1,30 @@
 age = int(input("Your age is: "))
-if age >0 and age <18:
-    print("Minor")
-elif age >=18 and age < 50:
-    print("Eligible")
-elif age >= 50:
-    print("Not Eligible")
+
+is_member = bool(int(input("Are you a member? (1 for Yes, 0 for No): ")))
+
+if age >6 and age <22:
+    if is_member:
+        print("You get a 30% discount!")
+    else:
+        print("You get a 15% discount!")
+
+elif age >=22 and age <65:
+    if is_member:
+        print("You get a 20% discount!")
+    else:
+        print("You get standard 5% discount!")
+
+elif age >=65 and age < 100:
+    if is_member:
+        print("You get a 30% discount!")
+    else:
+        print("You get a 15% discount!")
+
+elif age >= 100:
+    if is_member:
+        print("You get a 50% discount!")
+    else:
+        print("You get a 25% discount!")
+
 else:
-    print("Invalid Age And Age cannot be Negative")
+    print("Invalid age entered")
